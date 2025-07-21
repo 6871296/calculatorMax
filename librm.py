ba7623c0ba495b45219be019becee1b62870d5fe
6871296 <guojiacheng1022@163.com>
2025-07-21 10:57:13 +0800

    Update librm.py
    fixed phonics errors

--- librm.py
+++ librm.py
@@ -5,11 +5,11 @@ libs={"easygui":False,"simpleeval":False} #所有库是否已卸载成功
 for i in range(1,max):
     for l in libs:
         if libs[l]:
-            subprocess.run(sys.executable,"-m","pip","uninstall",l]
+            subprocess.run([sys.executable,"-m","pip","uninstall",l])
             try:
                 eval("import "+l)
-            except importError:
-                print("Sucsessfully uninstalled "+l")
+            except ImportError:
+                print("Sucsessfully uninstalled "+l)
                 libs[i]=True
     if False not in libs.values():
         print("Sucsessfully uninstalled "+len(libs)+" libraries")
